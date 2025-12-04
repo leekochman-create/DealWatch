854426cf857b5d70c046b2900363bb4bf0b7be61
leekochman-create <leekochman@gmail.com>
2025-12-04 13:02:02 +0200

    Create route.ts

---
+++ app/api/detect-country/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+import { getCountryFromIP } from "@/lib/getCountryFromIP";
+
+export async function GET(req: Request) {
+  const ip = req.headers.get("x-forwarded-for") || "8.8.8.8";
+  const country = await getCountryFromIP(ip);
+
+  return NextResponse.json({ country });
+}
